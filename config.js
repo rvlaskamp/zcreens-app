0ef13ec4ec4b11a7cb23b7f1a6fb3cabc65d99d8
rvlaskamp <rvlaskamp@gmail.com>
2017-05-01 20:41:12 +0200

    Added AminoGFX app

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ *  Configuration
+ */
+const configuration = {};
+
+/*
+  Colors
+ */
+
+/*
+  Dimensions
+ */
+
+configuration.dimensions = {
+  screenWidth: 1024,
+  screenHeight: 768
+}
+
+module.exports = configuration;
---
+++ config/dimensions.js
@@ -0,0 +1,8 @@
+const dimensions = {
+  app: {
+    width: 1920,
+    height: 1080,
+  },
+};
+
+module.exports = dimensions;
