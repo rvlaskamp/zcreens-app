f0a0275c0a5060644a7795ede93b729e4205f92b
Rene Vlaskamp <r.vlaskamp@nerds.company>
2017-05-04 08:04:46 +0200

    Aminogfx

--- app/start.sh
+++ app/start.sh
@@ -5,5 +5,4 @@
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
 
-rm /tmp/.X0-lock &>/dev/null || true
-startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+echo "Hello World"
