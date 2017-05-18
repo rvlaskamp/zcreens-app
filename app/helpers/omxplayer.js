a045fe1ef7d73f7232f0c3eb47f2ea14615050d4
rvlaskamp <rvlaskamp@gmail.com>
2017-05-18 22:01:16 +0200

    OMXPlayer

--- app/helpers/omxplayer.js
+++ app/helpers/omxplayer.js
@@ -2,19 +2,27 @@ const omxmanager = require('omx-manager');
 
 function omxplayer() {
   this.playerManager = new omxmanager();
+  this.player = null;
   this.playing = false;
 }
 
 omxplayer.prototype.play = function(url) {
-  this.player = null;
+  if (this.player && !this.playing) {
+    this.player.stop();
+    this.player = null;
+  }
+
   this.player = this.playerManager.create(url);
   this.player.play();
   this.playing = true;
 }
 
 omxplayer.prototype.stop = function() {
-  this.player = null;
-  this.playing = false;
+  if (this.player && this.playing) {
+    this.player.stop();
+    this.player = null;
+    this.playing = false;
+  }
 }
 
 omxplayer.prototype.isPlaying = function() {
