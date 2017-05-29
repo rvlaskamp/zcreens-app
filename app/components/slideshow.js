d91d9b79458137675402dc6f02ba07d8a140fa24
rvlaskamp <rvlaskamp@gmail.com>
2017-05-29 22:30:51 +0200

    Slideshow animation reverted

--- app/components/slideshow.js
+++ app/components/slideshow.js
@@ -14,9 +14,6 @@ const slideshowComponent = function(app, images) {
   this.slideshowTimer = null;
   this.playing = false;
 
-  this.slideshowCurrent = 0;
-  this.slideshowNext = 1;
-
   // Create slideshowGroup
   this.group = this.app.createGroup();
   this.group.w(this.app.w());
@@ -33,43 +30,25 @@ slideshowComponent.prototype.play = function() {
     }
 
     this.playing = true;
+    // Create first pictures for slideshow
+    const picture1 = pictureComponent(this.app, this.slideshow.next());
 
-    this.picture1 = pictureComponent(this.app, this.slideshow.next());
-    this.imageGroup1 = this.app.createGroup();
-    this.imageGroup1.w(this.app.w());
-    this.imageGroup1.h(this.app.h());
-    this.imageGroup1.x(0);
-    this.imageGroup1.y(0);
-    this.imageGroup1.opacity(1);
-    this.imageGroup1.add(this.picture1);
+    this.group.add(picture1);
 
-    this.picture2 = pictureComponent(this.app, this.slideshow.next());
-    this.imageGroup2 = this.app.createGroup();
-    this.imageGroup2.w(this.app.w());
-    this.imageGroup2.h(this.app.h());
-    this.imageGroup2.x(0);
-    this.imageGroup2.y(0);
-    this.imageGroup2.opacity(0);
-    this.imageGroup2.add(this.picture2);
-
-    this.group.add(this.imageGroup1);
-    this.group.add(this.imageGroup2);
-
-    this.group.opacity(1);
+    this.group.opacity.anim().from(0).to(1).dur(250).start();
 
     this.slideshowTimer = setInterval(() => {
+      const picture1 = this.group.children[0];
+      const picture2 = pictureComponent(this.app, this.slideshow.next());
+      picture2.opacity(0);
 
-      this.group.children[this.slideshowCurrent].opacity.anim().from(1).to(0).dur(500).start();
+      this.group.add(picture2);
 
-      this.group.children[this.slideshowNext].opacity.anim().from(0).to(1).dur(500).then(() => {
-        if (this.slideshowNext === 0) {
-          this.slideshowNext = 1;
-          this.slideshowCurrent = 0;
-        } else {
-          this.slideshowNext = 0;
-          this.slideshowCurrent = 1;
-        }
-        this.group.children[this.slideshowNext].children[0].src(this.slideshow.next());
+
+      picture1.opacity.anim().from(0).to(1).dur(250);
+      picture2.opacity.anim().from(0).to(1).dur(500).then(() => {
+        this.group.remove(picture1);
+        picture1.destroy();
       }).start();
     }, 10000);
   }
@@ -79,13 +58,13 @@ slideshowComponent.prototype.stop = function() {
   clearInterval(this.slideshowTimer);
 
   if (this.group.children.length > 0) {
+    const picture = this.group.children[0];
 
     this.group.opacity.anim().from(1).to(0).dur(250).then(() => {
       this.group.clear();
-      this.imageGroup1.clear();
-      this.imageGroup2.clear();
-      this.picture1.destroy();
-      this.picture2.destroy();
+      if (picture) {
+        picture.destroy();
+      }
     }).start();
   }
 }
